3b0896600ec78f0fca7d6c2eb758f18aebb3c36c
Swakshath <swakshath.rajkumar@yahoo.in>
2021-03-16 16:27:49 +0530

    Base homepage and node structure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port=8080;
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+const cors = require('cors');
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.use("/styles",express.static(__dirname + "/styles"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(expressLayout);
+
+app.use('/',require('./routes/router.js'));
+
+app.listen(port,function(){
+    console.log("Started1 running");
+});
